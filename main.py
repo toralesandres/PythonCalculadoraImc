e1d8a1111862c952207ee0e84233c1df039f78e1
Torales Andres <andres.toraless@gmail.com>
2022-04-16 08:43:22 -0300

    se agrego funsiones

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-peso = int(input('Ingrese su peso en kg'));
-alturaEnCm = int(input('Ingrese su altura en cm'));
-alturaEnMetros = alturaEnCm /100;
-imc = peso / (alturaEnMetros * alturaEnMetros);
+#calculadora de imc, con peso y altura
 
+def calcularImc(peso, alturaEnMetros):
+    imc = peso / (alturaEnMetros * alturaEnMetros)
+    return imc
 
-if imc <= 19:
-    print('estado de delgadez');
-if imc >= 20 and imc <=26:
-    print('peso normal');
-if imc >= 26 and imc <=30:
-    print('estado de sobrepeso');
-if imc >=30:
-    print('estado de obesidad');
+def pedirImc():
+    peso = float(input('Ingrese su peso en kg'));
+    alturaEnCm = int(input('Ingrese su altura en cm'));
+    alturaEnMetros = alturaEnCm /100;
 
+    imc = calcularImc(peso, alturaEnMetros)
+    if imc <= 19:
+        print('estado de delgadez');
+    if imc >= 20 and imc <=26:
+        print('peso normal');
+    if imc >= 26 and imc <=30:
+        print('estado de sobrepeso');
+    if imc >=30:
+        print('estado de obesidad');
+    print('si IMC es de: ' + str(imc));
 
+pedirImc();
